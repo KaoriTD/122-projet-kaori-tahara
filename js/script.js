69988e4b4d0846b7c729c2356883fb3fd6ef28a5
Kaori Tahara <kaori.tahara@divcom.ch>
2026-04-09 11:57:37 +0200

    feat: ajouter des données de personnages et les afficher dans la liste

--- js/script.js
+++ js/script.js
@@ -1,4 +1,130 @@
 "use strict";
 
 // Tableau de données — à générer avec Copilot / une IA
-const data = [];
+//Rating by https://www.sanrio.co.jp/special/characterranking/2025/en/result/
+const data = [
+    {
+        id: 1,
+        name: "Hello Kitty",
+        category: "Cat",
+        year: 1974,
+        ranking: 5,
+        image: "img/hello-kitty.png",
+    },
+    {
+        id: 2,
+        name: "Cinnamoroll",
+        category: "Dog",
+        year: 2001,
+        ranking: 2,
+        image: "img/cinnamoroll.png",
+    },
+    {
+        id: 3,
+        name: "My Melody",
+        category: "Rabbit",
+        year: 1975,
+        ranking: 6,
+        image: "img/my-melody.png",
+    },
+    {
+        id: 4,
+        name: "Pompompurin",
+        category: "Dog",
+        year: 1996,
+        ranking: 1,
+        image: "img/pompompurin.png",
+    },
+    {
+        id: 5,
+        name: "Kuromi",
+        category: "Rabbit",
+        year: 2005,
+        ranking: 4,
+        image: "img/kuromi.png",
+    },
+    {
+        id: 6,
+        name: "Keroppi",
+        category: "Frog",
+        year: 1988,
+        ranking: 14,
+        image: "img/keroppi.png",
+    },
+    {
+        id: 7,
+        name: "Bad Badtz-Maru",
+        category: "Bird",
+        year: 1993,
+        ranking: 11,
+        image: "img/bad-badtz-Maru.png",
+    },
+    {
+        id: 8,
+        name: "Little Twin Stars",
+        category: "Fairy",
+        year: 1975,
+        ranking: 7,
+        image: "img/little-twin-stars.png"
+    },
+    {
+        id: 9,
+        name: "Tuxedosam",
+        category: "Bird",
+        year: 1979,
+        ranking: 9,
+        image: "img/tuxedosam.png"
+    },
+    {
+        id: 10,
+        name: "Pochacco",
+        category: "Dog",
+        year: 1989,
+        ranking: 3,
+        image: "img/pochacco.png",
+    },
+    {
+        id: 11,
+        name: "Hangyodon",
+        category: "Mermen",
+        year: 1985,
+        ranking: 8,
+        image: "img/hangyodon.png",
+    },
+    {
+        id: 12,
+        name: "Pekkle",
+        category: "Bird",
+        year: 1990,
+        ranking: 10,
+        image: "img/pekkle.png",
+    },
+    {
+        id: 13,
+        name: "Gudetama",
+        category: "Egg",
+        year: 2013,
+        ranking: 16,
+        image: "img/gudetama.png",
+    },
+    {
+        id: 14,
+        name: "Aggretsuko",
+        category: "Red Panda",
+        year: 2015,
+        ranking: 36,
+        image: "img/aggretsuko.png",
+    }
+];
+
+//Récupère la liste #liste
+const ulList = document.getElementById("list");
+
+//Parcours la liste et crére un li par character
+data.forEach(item => {
+    ulList.innerHTML += `
+        <li>
+            <div>${item.name}</div>
+            <div><img src="${item.image}" alt="${item.name}"></div>
+        </li>`;
+});
